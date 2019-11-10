9ed24875b8b20ffbdfe38bf6c7f89e6a5091635b
Admin <admin@localhost.localdomain>
2019-11-11 03:39:02 +0530

    latest

---
+++ Yoga01/app/src/main/java/com/example/yoga01/StartActivity.kt
@@ -0,0 +1,26 @@
+package com.example.yoga01
+
+import android.content.Intent
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+        var beginner:ImageView=findViewById(R.id.beginner)
+        var intermediate:ImageView=findViewById(R.id.intermediate)
+        var expert:ImageView=findViewById(R.id.expert)
+
+        beginner.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("label","Beginner")
+            startActivity(intent)
+        }
+    }
+}

d02d990f33d5002d8aed8ba6c6fc67fe2c05af6c
ShwetaKauthankar <sweta.kauthankar@gmail.com>
2019-11-19 02:59:19 +0530

    menu

--- Yoga01/app/src/main/java/com/example/yoga01/MenuActivity.kt
+++ Yoga01/app/src/main/java/com/example/yoga01/MenuActivity.kt
@@ -1,44 +1,21 @@
 package com.example.yoga01
 
-import android.content.Intent
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_menu.*
 
-class StartActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener
+{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_menu)
 
-        var beginner:ImageView=findViewById(R.id.beginner)
-        var intermediate:ImageView=findViewById(R.id.intermediate)
-        var expert:ImageView=findViewById(R.id.expert)
-
-        beginner.setOnClickListener {
-            var intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("label","Beginner")
-            startActivity(intent)
-        }
-
-        intermediate.setOnClickListener {
-            var intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("label","intermediate")
-            startActivity(intent)
-        }
-
-        expert.setOnClickListener {
-            var intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("label","expert")
-            startActivity(intent)
-        }
         setSupportActionBar(toolbar)
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, 0, 0
@@ -60,6 +37,7 @@ class StartActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
             R.id.nutrition -> {
                 Toast.makeText(this, "Newsletter", Toast.LENGTH_SHORT).show()
             }
+
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
@@ -73,4 +51,5 @@ class StartActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
             super.onBackPressed()
         }
     }
+
 }
